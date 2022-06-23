873f9d36ed44f34b85b58b30470761d7596f175e
MirkOrlando <mirko@orlando-m.it>
2022-06-23 15:16:51 +0200

    create foreign key and migrate in the posts table

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name', 'slug',];
+
+    /**
+     * Get all of the post for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function post(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'content', 'cover_img', 'slug'];
+    protected $fillable = ['title', 'content', 'cover_img', 'slug', 'cat_id'];
+
+    /**
+     * Get the category that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
