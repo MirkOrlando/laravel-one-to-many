ffd3592723301681867dc467ff4cd7fb8d3f4ef8
MirkOrlando <mirko@orlando-m.it>
2022-06-23 17:20:15 +0200

    create categories.store method

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\admin;
 use App\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
@@ -20,46 +22,19 @@ class CategoryController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CategoryRequest $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
-    {
-        //
+        $val_data = $request->all();
+        $slug = Str::slug($request->name);
+        $val_data['slug'] = $slug;
+        //dd($val_data);
+        Category::create($val_data);
+        return redirect()->back()->with('message', "Category \"$slug\" created successfulli");
     }
 
     /**
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,5 +1,62 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>All Categories</h1>
+    <div class="container">
+        <h1>All Categories</h1>
+        <div class="row">
+            <div class="col">
+                <form action="{{ route('admin.categories.store') }}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                            id="name" aria-describedby="nameHelper" placeholder="New Category Name"
+                            value="{{ old('name') }}">
+                        <small id="nameHelper" class="form-text text-muted">Add a New Category</small>
+                        @error('name')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary">Add</button>
+                </form>
+            </div>
+            <div class="col">
+                <table class="table table-striped table-inverse table-responsive">
+                    <thead class="thead-inverse">
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Posts Count</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($categories as $category)
+                            <tr>
+                                <td scope="row">{{ $category->id }}</td>
+                                <td>
+                                    <form id="category-{{ $category->id }}"
+                                        action="{{ route('admin.categories.update', $category->slug) }}" method="post">
+                                        @csrf
+                                        <input class="border-0 bg-transparent" type="text" name="name" id="name"
+                                            value="{{ $category->name }}">
+                                    </form>
+                                </td>
+                                <td>{{ count($category->posts) }}</td>
+                                <td>
+                                    Update - Delete
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td scope="row">
+                                    <p>No Categories found! Add your first Category</p>
+                                </td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
 @endsection
